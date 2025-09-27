dce06092604eac33b9db0bb7f66b08c26e049c39
Gilles FIDANI <gillesfidani@gmail.com>
2025-09-27 20:11:19 +0200

    cleaning

---
+++ R/data.R
@@ -0,0 +1,19 @@
+#' TOC Eurostat 2025-09-27
+#'
+#' Dataset contenant des infos sur les donnees dispo sur eurosat
+#'  Les variables sont les suivantes
+#'
+#' @format A data frame with x rows and y variables:
+#' \describe{
+#'   \item{title}{title}
+#'   \item{code}{code}
+#'   \item{type}{type}
+#'   \item{last.update.of.data}{last update of data}
+#'   \item{last.table.structure.change}{last table structure change}
+#'   \item{data.start}{data start}
+#'   \item{data.end}{data end}
+#'   \item{values}{values}
+#'   \item{hierarchy}{hierarchy}
+#' }
+"tocestatfr20250927"
+
